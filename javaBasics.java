7930fcb99e147742f40d5fcb563278996b4cbf17
1amAdityaa <adityaraj76692@gmail.com>
2025-01-21 18:29:10 +0530

    average

--- javaBasics.java
+++ javaBasics.java
@@ -2,8 +2,10 @@ public class javaBasics
 {  
 public static void main(String[] args) {
     System.out.println("hello world!");
+    int a = 10;
+    int b = 5;
+    System.out.println(a);
+    System.out.println(b);
     
 }
-    
-
 }
